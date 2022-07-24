6cab52be4652dc29c2a7fb37cabce23a12864d59
dangkl123 <nhatdang2604@gmail.com>
2022-07-24 19:59:28 +0700

    Before writing getNextBackend() method in svpool.go

--- src/svpool.go
+++ src/svpool.go
@@ -2,9 +2,20 @@ package svpool
 
 import (
 	"./backend"
+	"sync/atomic"
 )
 
 type ServerPool struct {
 	Backends	[]*Backend
 	Current		uint64
-}`
+}
+
+func (sp *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&sp.Current, uint64(1)) % uint64(len(s.Backends)))
+}
+
+//Return next alive backend to take a connection
+func (sp *ServerPool) GetNextBackend() *Backend {
+
+	//TODO: 
+}
