29c2ee058dcb5d7747b444bc56d63ee2a07f4ef2
dangkl123 <nhatdang2604@gmail.com>
2022-07-25 20:30:00 +0700

    Add LoadBalance() method and the server entity

--- src/main.go
+++ src/main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"./backend"
-	"./svpool"
+	"./svpool"	
+	"net/http"
 )
+
+const (
+	PORT = "8088"
+)
+
+var (
+	serverPool = svpool.ServerPool
+
+	server = http.Server {
+		Addr:		PORT
+		Handler:	http.HanlderFunc(loadBalance)
+	}
+)
+
+//Load balances the incoming request
+func LoadBalance(writer http.ResponseWriter, request *http.Request) {
+	selectedBackend := serverPool.GetNextBackend()
+	
+	//Throw the error, if all the backends is down
+	if nil == selectedBackend {
+		http.Error(writer, "Service is not available", http.StatusServiceUnavailable)	
+		return
+	}
+
+	//Else, pass the request to the backend
+	selectedBackend.ReverseProxy.ServeHTTP(writer, request)
+}
 
 func main (
 
