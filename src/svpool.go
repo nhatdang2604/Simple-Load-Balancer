ec160fc85fc6d2e9ae0194e1b49027fbaeef060b
dangkl123 <nhatdang2604@gmail.com>
2022-07-25 20:04:39 +0700

    Add GetNextBackend() method

--- src/svpool.go
+++ src/svpool.go
@@ -17,5 +17,24 @@ func (sp *ServerPool) NextIndex() int {
 //Return next alive backend to take a connection
 func (sp *ServerPool) GetNextBackend() *Backend {
 
-	//TODO: 
+	//Loop entier backends to find out an alive backend
+	next := sp.NextIndex();
+	size := len(sp.Backends)
+	end := size + next;		//start from the 'next' and move a full cycle
+
+	for i := next; i < end; ++i {
+		idx := i % size		//the index of the backend in sp.Backends
+
+		//If the tested backend is alive
+		// => choose the backend
+		if s.Backends[idx].Alive {
+			if i != next {
+				atomic.StoreUint64(&sp.Current, uint64(idx))
+			}
+			return sp.Backends[idx]
+		}
+	}
+
+	//Return nil if all the backends are down
+	return nil
 }
